84c3cbe1172c08d984a14cd54e5a7cb1038404e8
MomentSum <momentsum@163.com>
2024-09-16 16:12:45 +0800

    重构：新的骑兵马贴图

--- scenes/characters/cavalry.gd
+++ scenes/characters/cavalry.gd
@@ -7,3 +7,12 @@ func _on_cavalry_ai_dashing_began() -> void:
 
 func _on_cavalry_ai_dashing_ended() -> void:
 	%Hitbox.disabled = true
+
+func _process(delta: float) -> void:
+	if is_instance_valid(target):
+		if target.position.x < position.x:
+			$Horse.scale.y = -$Horse.scale.x
+			$Horse.rotation = PI
+		else:
+			$Horse.scale.y = $Horse.scale.x
+			$Horse.rotation = 0
