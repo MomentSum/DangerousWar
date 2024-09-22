1245b4fc348df6c1ec54c2458804ef5e04c6249a
MomentSum <momentsum@163.com>
2024-09-22 12:02:47 +0800

    杂项：删除了弓箭手近程发射线射弹的逻辑

--- scenes/characters/archer.gd
+++ scenes/characters/archer.gd
@@ -4,7 +4,4 @@ extends Character
 
 
 func _on_movement_ai_attack() -> void:
-	if position.distance_to(target.position) < $MovementAI.target_distance_min * 1.5:
-		%LinearArrowLauncher.launch(target.position)
-	else:
-		%ArrowLauncher.launch(target.position)
+	%ArrowLauncher.launch(target.position)
