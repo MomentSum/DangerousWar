4dcb6056eb56e199fd043a8b2b754758eebf1fc3
MomentSum <momentsum@163.com>
2024-10-26 17:28:48 +0800

    杂项

--- scenes/characters/berserker.gd
+++ scenes/characters/berserker.gd
@@ -1,7 +1,5 @@
 extends Character
 
 
-
-
 func _on_movement_ai_attack() -> void:
 	$AnimationPlayer.play("attack")
--- scenes/characters/ghost.gd
+++ scenes/characters/ghost.gd
@@ -5,4 +5,3 @@ extends Character
 func _on_movement_ai_attack() -> void:
 	if is_instance_valid(target):
 		target.hurtbox.be_attacked(damage, 1)
-		queue_free()
