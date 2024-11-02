460f6c202a2ad659f25323b532bc902c59ebf92f
MomentSum <momentsum@163.com>
2024-11-02 22:48:16 +0800

    杂项：沙盒模式运行时斜线会隐藏

--- scenes/preload/debug_info.gd
+++ scenes/preload/debug_info.gd
@@ -2,12 +2,8 @@ extends Control
 
 
 func _ready() -> void:
-	ProjectSettings.settings_changed.connect(update)
-	update()
+	$VersionLabel.text ="V" + str(ProjectSettings.get_setting("application/config/version"))
 
 
 func _process(_delta: float) -> void:
 	$FPSLabel.text = "FPS:" + str(Performance.get_monitor(Performance.TIME_FPS))
-
-func update() -> void:
-	$FPSLabel.visible = ProjectSettings.get_setting("game/debug/fps_display", false)
